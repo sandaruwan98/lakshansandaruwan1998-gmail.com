be16f5cf072ce4bd01b58ff9e29482eb5ec756c5
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-04-08 11:41:49 +0530

    new nav bar and custom animated logout menu

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const logo = document.querySelector("#logo");
 const hamburger = document.querySelector(".hamburger");
 const slider = document.querySelector(".slider");
 
+const logmenu1 = document.querySelector(".logmenu1");
+const menubtns = document.querySelectorAll(".menubtn");
+const menutrigger = document.querySelector(".menutrigger");
+
+
+menutrigger.addEventListener('click', () => {
+    logmenu1.classList.toggle('open');
+    menubtns.forEach(bt => {
+        bt.classList.toggle('fade');
+    });
+});
 
 const tl = new TimelineMax();
 
 tl
     .fromTo(hero, 0.8, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-    .fromTo(hero, 1, { width: "100%" }, { width: "80%", ease: Power2.easeInOut })
-    .fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut },"-=1.2")
-    .fromTo(logo, 0.5, { x: 30 ,opacity: 0}, { x: 0,opacity: 1 },"-=0.5")
-    .fromTo(hamburger, 0.5, { x: 30 ,opacity: 0}, { x: 0,opacity: 1 },"-=0.5");
+    .fromTo(hero, 1, { width: "100%", height: "80%" }, { width: "80%", height: "100%", ease: Power2.easeInOut })
+    .fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
+    .fromTo(logo, 0.5, { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.5")
+    .fromTo(hamburger, 0.5, { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.5");
--- index.php
+++ index.php
@@ -24,63 +24,182 @@ if (!isset($_SESSION["username"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link rel="stylesheet" href=""> -->
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="css/mdb.min.css">
+    <!-- Your custom styles (optional) -->
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
     <link rel="stylesheet" href="./cssLogin/style.css">
+
     <link rel="stylesheet" href="./cssLogin/menu.css">
 </head>
 
 <body>
 
     <header>
-        <nav>
+        <div class="logmenu1">
+            <div class="logmenu">
+                <!-- <button class="btn-pr"></button> -->
+                <div class="menubtn">
+                    <a href="" class="btn btn-dark btn-lg">My Account</a>
+                </div>
+                <div class="menubtn">
+                    <a href="index.php?logout='1'" role="button" class="btn btn-dark btn-lg">Logout</a>
+                </div>
+
+            </div>
+        </div>
+
+        <nav class="mb-1 navbar navbar-expand-lg navbar-dark indigo fixed-top scrolling-navbar">
+            <a class="navbar-brand" href="#"> <?php if (isset($_SESSION["username"])) :
+
+                                                ?>
+                Welcome <?php echo $_SESSION["username"] ?>
+
+                <?php endif ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">Dropdown
+                        </a>
+                        <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto nav-flex-icons">
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link waves-effect waves-light">
+                            <i class="fab fa-google-plus-g"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown menutrigger">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-user"></i> Profile </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-info"
+                            aria-labelledby="navbarDropdownMenuLink-4">
+                            <a class="dropdown-item" href="#">My account</a>
+                            <a class="dropdown-item" href="#">Log out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- <nav>
             <?php if (isset($_SESSION["username"])) :
 
             ?>
                 <h3 id="logo">Welcome <?php echo $_SESSION["username"] ?> </h3>
 
             <?php endif ?>
-            <!-- <img src="./img/hamburger.png" alt="ffgg" class="hamburger"> -->
-            <div class="hamburger">
-            <input type="checkbox" id="hi">
-            <label class="menu" for="hi">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-        </div>
-            </label>
-        </nav>
+            <ul class="nav-links">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Projects</a></li>
+            </ul>
+             <img src="./img/hamburger.png" alt="ffgg" class="hamburger"> 
+
+
+        </nav> -->
+
         <section>
             <div class="hero">
                 <img src="./img/carwall.jpg" alt="">
                 <h1 class="headline">Dream big</h1>
             </div>
+
         </section>
     </header>
 
     <div class="slider"></div>
     <div class="p">
-        <a class="btn btn-primary btn-lg" href="index.php?logout='1'" role="button">Logout</a>
-       
+        <!-- <a class="btn btn-primary btn-lg" href="index.php?logout='1'" role="button">Logout</a> -->
+        <!-- <div class="hamburger">
+            <input type="checkbox" id="hi">
+            <label class="menu" for="hi">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </label>
+        </div> -->
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem nisi sit quibusdam totam vero consectetur ipsum eligendi rerum veniam ut, eaque ratione. Earum alias necessitatibus nulla voluptatum, molestiae ex error.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur cumque sit animi magnam officia, laudantium magni voluptatibus, consectetur aperiam nostrum, sunt praesentium iste vel commodi nemo laborum quas voluptatum.
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem nisi sit quibusdam totam vero consectetur
+            ipsum eligendi rerum veniam ut, eaque ratione. Earum alias necessitatibus nulla voluptatum, molestiae ex
+            error.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur cumque sit animi magnam officia,
+            laudantium magni voluptatibus, consectetur aperiam nostrum, sunt praesentium iste vel commodi nemo laborum
+            quas voluptatum.
         </p>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet nulla quidem ad pariatur incidunt, porro odio repudiandae iste aspernatur! Quos quasi, quibusdam molestiae nisi est soluta id facere quo?</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem nisi sit quibusdam totam vero consectetur ipsum eligendi rerum veniam ut, eaque ratione. Earum alias necessitatibus nulla voluptatum, molestiae ex error.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur cumque sit animi magnam officia, laudantium magni voluptatibus, consectetur aperiam nostrum, sunt praesentium iste vel commodi nemo laborum quas voluptatum.
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet nulla quidem ad pariatur
+            incidunt, porro odio repudiandae iste aspernatur! Quos quasi, quibusdam molestiae nisi est soluta id facere
+            quo?</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem nisi sit quibusdam totam vero consectetur
+            ipsum eligendi rerum veniam ut, eaque ratione. Earum alias necessitatibus nulla voluptatum, molestiae ex
+            error.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur cumque sit animi magnam officia,
+            laudantium magni voluptatibus, consectetur aperiam nostrum, sunt praesentium iste vel commodi nemo laborum
+            quas voluptatum.
         </p>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet nulla quidem ad pariatur incidunt, porro odio repudiandae iste aspernatur! Quos quasi, quibusdam molestiae nisi est soluta id facere quo?</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem nisi sit quibusdam totam vero consectetur ipsum eligendi rerum veniam ut, eaque ratione. Earum alias necessitatibus nulla voluptatum, molestiae ex error.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur cumque sit animi magnam officia, laudantium magni voluptatibus, consectetur aperiam nostrum, sunt praesentium iste vel commodi nemo laborum quas voluptatum.
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet nulla quidem ad pariatur
+            incidunt, porro odio repudiandae iste aspernatur! Quos quasi, quibusdam molestiae nisi est soluta id facere
+            quo?</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem nisi sit quibusdam totam vero consectetur
+            ipsum eligendi rerum veniam ut, eaque ratione. Earum alias necessitatibus nulla voluptatum, molestiae ex
+            error.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur cumque sit animi magnam officia,
+            laudantium magni voluptatibus, consectetur aperiam nostrum, sunt praesentium iste vel commodi nemo laborum
+            quas voluptatum.
         </p>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet nulla quidem ad pariatur incidunt, porro odio repudiandae iste aspernatur! Quos quasi, quibusdam molestiae nisi est soluta id facere quo?</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet nulla quidem ad pariatur
+            incidunt, porro odio repudiandae iste aspernatur! Quos quasi, quibusdam molestiae nisi est soluta id facere
+            quo?</h1>
     </div>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" integrity="sha256-lPE3wjN2a7ABWHbGz7+MKBJaykyzqCbU96BJWjio86U=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js" integrity="sha256-fIkQKQryItPqpaWZbtwG25Jp2p5ujqo/NwJrfqAB+Qk=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"
+        integrity="sha256-lPE3wjN2a7ABWHbGz7+MKBJaykyzqCbU96BJWjio86U=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TimelineMax.min.js"
+        integrity="sha256-fIkQKQryItPqpaWZbtwG25Jp2p5ujqo/NwJrfqAB+Qk=" crossorigin="anonymous"></script>
 
     <script src="app.js"></script>
+
+
+
+    
 </body>
 <?php
 
